b273614decad8dd215c2089f59c2677d0e75476b
Dawid <dawidson2008@o2.pl>
2024-04-17 18:17:40 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full min-h-dvh">
+      <h1 className="text-3xl font-semibold uppercase">Holtzetreppen</h1>
+    </div>
+  );
+};
+
+export default App;
